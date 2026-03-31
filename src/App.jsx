2f5911905d4d94ce9974f226b858af2ba48bf199
Deep Mandloi <mandloideep22@gmail.com>
2026-03-30 21:35:43 -0500

    feat: update App component title to include class designation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,57 @@
+import Button from "./button";
+import DangerButton from "./dangerButton";
+import Layout from "./layout";
+import { useState } from "react";
+import LifecycleDemo from "./lifecycle";
+import WindowWidth from "./window";
+
 function App() {
-  return <h1>CS 300</h1>;
+  const [, setShow] = useState(true);
+
+  function toggleShow() {
+    setShow((prevShow) => !prevShow);
+  }
+
+  return (
+    <Layout
+      header={<h1>Header</h1>}
+      footer={<h1>Footer</h1>}
+    >
+      <p>This is the body.</p>
+
+      <Button
+        variant="primary"
+        onClick={() => alert("Primary button clicked!")}
+      >
+        Primary Button
+      </Button>
+
+      <Button
+        variant="secondary"
+        onClick={() => alert("Secondary button clicked!")}
+      >
+        Secondary Button
+      </Button>
+
+      <DangerButton onClick={() => alert("Danger button clicked!")}>
+        Danger Button
+      </DangerButton>
+
+      <div>
+        <Button
+          variant="secondary"
+          onClick={toggleShow}
+        >
+          Toggle Show
+        </Button>
+
+        <WindowWidth
+          message="Hello, World!"
+          delay={1000}
+        />
+      </div>
+    </Layout>
+  );
 }
 
 export default App;
